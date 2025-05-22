d638c44428419407cf40cd1a3491cac041a5c194
Lorenzo Ferri <lorenzo.ferri96+github@gmail.com>
2025-05-22 17:24:36 +0200

    Change to static and forks

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let linkerSettings: [LinkerSetting] = [
 let package = Package(
     name: "swift-windowsappsdk",
     products: [
-        .library(name: "WinAppSDK", type: .dynamic, targets: ["WinAppSDK"]),
+        .library(name: "WinAppSDK", type: .static, targets: ["WinAppSDK"]),
         .library(name: "CWinAppSDK", targets: ["CWinAppSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/thebrowsercompany/swift-cwinrt", branch: "main"),
-        .package(url: "https://github.com/thebrowsercompany/swift-uwp", branch: "main"),
-        .package(url: "https://github.com/thebrowsercompany/swift-windowsfoundation", branch: "main"),
+        .package(url: "https://github.com/LorenzoFerri/swift-cwinrt", branch: "main"),
+        .package(url: "https://github.com/LorenzoFerri/swift-uwp", branch: "main"),
+        .package(url: "https://github.com/LorenzoFerri/swift-windowsfoundation", branch: "main"),
     ],
     targets: [
         .target(
